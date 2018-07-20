8188ba60b8d57a5e77845290b3b3050ff3a0ebc7
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-20 16:55:29 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,6 +17,9 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #     :memorial_day => ["BBQ"]
   #   }
   # }
+  holiday_hash[:summer][:fourth_of_july][1]
+
+#  binding.pry
   # return the second element in the 4th of July array
 end
 
@@ -24,25 +27,30 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+holiday_hash[:winter].each do |holiday, supplies|
+  supplies << supply
+#  binding.pry
+  end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+holiday_hash[:winter].collect do |holiday, supplies|
+  supplies
+  end.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -54,17 +62,23 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
-end
+    holiday_hash.each do |season, holidays|
+      puts "#{season.capitalize}:"
+      holidays.each do |holiday, supplies|
+        puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
+      end
+    end
+  end
+
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+holiday_hash.map do |season, holidays|
+  holidays.map do |holiday, supplies|
+    holiday if supplies.include?("BBQ")
 
+  #binding.pry
+    end
+  end.flatten.compact
 end
-
-
-
-
-
-
-
