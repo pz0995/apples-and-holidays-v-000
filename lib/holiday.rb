56d5141b06f3bfeef910c0640063d65e59aa4b61
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-20 14:55:09 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,6 +69,8 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 holiday_hash.map do |season, holidays|
   holidays.map do |holiday, supplies|
+    holiday if supplies.include?("BBQ")
+
   binding.pry
 end
 end
